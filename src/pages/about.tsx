3902732196b254454e81274883305f046d328d05
scott <scottvaughn5@gmail.com>
2024-05-08 13:53:03 -0600

    init

---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function About() {
+  return (
+    <Layout>
+      <h1>About me</h1>
+      <p>This is a React page about me.</p>
+    </Layout>
+  );
+}
