f4f7eddf9d7e04efab01753434ca4fdad6e781ff
scott <scottvaughn5@gmail.com>
2024-05-08 13:57:19 -0600

    github pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'scottvaughn5', // Usually your GitHub org/user name.
-  projectName: 'shofancy-blog', // Usually your repo name.
+  projectName: 'blog', // Usually your repo name.
   deploymentBranch: "main",
 
   onBrokenLinks: 'throw',
